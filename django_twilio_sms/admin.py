55f7a8bbd6f524f0ae4f7715cd03666a49255b04
Carl Kibler <carl.scharenberg@gmail.com>
2016-04-15 21:36:10 -0500

    Improve admin display

--- django_twilio_sms/admin.py
+++ django_twilio_sms/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Message)
-admin.site.register(Response)
-admin.site.register(Account)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('to_phone_number', 'from_phone_number', 'status', 'date_sent')
+    list_display_links = list_display
+    list_filter = ('status', 'date_sent')
+    date_hierarchy = 'date_sent'
+    ordering = ('-date_sent', )
+
+
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('action', 'active', 'body', 'date_updated')
+    list_display_links = list_display
+    list_filter = ('action', 'active')
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'owner_account_sid', 'account_type', 'status', 'date_updated')
+    list_display_links = list_display
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Response, ResponseAdmin)
+admin.site.register(Account, AccountAdmin)
